664111e181639a631d67fe37ff7a7a2090a31e8d
rikucherry1993 <rikucherry0522@gmail.com>
2021-09-19 19:23:29 +0900

    Delete unused database callback

--- app/src/main/java/com/rikucherry/simplesnake/SnakeGameApplication.kt
+++ app/src/main/java/com/rikucherry/simplesnake/SnakeGameApplication.kt
@@ -3,13 +3,9 @@ package com.rikucherry.simplesnake
 import android.app.Application
 import com.rikucherry.simplesnake.data.ScoreDataBase
 import com.rikucherry.simplesnake.data.ScoreRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
 
 class SnakeGameApplication : Application() {
 
-    val applicationScope = CoroutineScope(SupervisorJob())
-
-    val dataBase by lazy { ScoreDataBase.getDatabase(this, applicationScope)}
+    val dataBase by lazy { ScoreDataBase.getDatabase(this)}
     val repository by lazy { ScoreRepository(dataBase.scoreDao())}
 }
--- app/src/main/java/com/rikucherry/simplesnake/data/ScoreDataBase.kt
+++ app/src/main/java/com/rikucherry/simplesnake/data/ScoreDataBase.kt
@@ -4,38 +4,24 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 @Database(entities = [Score::class], version = 1, exportSchema = true)
 abstract class ScoreDataBase : RoomDatabase() {
     abstract fun scoreDao(): ScoreDao
 
-    private class ScoreDatabaseCallback (private val scope: CoroutineScope) : RoomDatabase.Callback() {
-        override fun onCreate(db: SupportSQLiteDatabase) {
-            super.onCreate(db)
-            INSTANCE?.let {
-                database -> scope.launch {
-                    //do nothing
-            }
-            }
-        }
-    }
-
     companion object {
+        private const val DB_NAME = "score_database"
+
         @Volatile
         private var INSTANCE: ScoreDataBase? = null
 
-        fun getDatabase(context: Context, scope: CoroutineScope): ScoreDataBase {
+        fun getDatabase(context: Context): ScoreDataBase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     ScoreDataBase::class.java,
-                    "score_database",
-                ).fallbackToDestructiveMigration()
-                    .allowMainThreadQueries() //todo: Due to "Cannot access database on the main thread since it may potentially lock the UI for a long period of time" bad! resolve later
-                    .addCallback(ScoreDatabaseCallback(scope))
+                    DB_NAME,
+                ).allowMainThreadQueries() //todo: Due to "Cannot access database on the main thread since it may potentially lock the UI for a long period of time" bad! resolve later
                     .build()
 
                 INSTANCE = instance
