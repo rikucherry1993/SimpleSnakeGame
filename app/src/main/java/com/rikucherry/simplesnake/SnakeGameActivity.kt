e38760514e83e4f774d313a53df56892e5edcba6
rikucherry1993 <rikucherry0522@gmail.com>
2021-09-29 22:47:21 +0900

    [WIP] Add screen share feature

--- app/src/main/java/com/rikucherry/simplesnake/SnakeGameActivity.kt
+++ app/src/main/java/com/rikucherry/simplesnake/SnakeGameActivity.kt
@@ -1,10 +1,19 @@
 package com.rikucherry.simplesnake
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.media.MediaScannerConnection
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_snake_game.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 class SnakeGameActivity : AppCompatActivity() {
     private val viewModel: GameViewModel by viewModels {
@@ -68,21 +77,26 @@ class SnakeGameActivity : AppCompatActivity() {
                         } else {
                             "GAME OVER!"
                         }
-                    )
-                        .setMessage(
+                    ).setMessage(
                             "Your score is: ${score_text.text}\n"
                                     + if (isBest) {
                                 ""
                             } else {
                                 "Current best record is: $lastBestScore"
                             }
-                        )
-                        .setNegativeButton("Quit") { _, _ ->
+                        ).setNegativeButton("Quit") { _, _ ->
                             finish()
                         }.setPositiveButton("Play again") { dialog, _ ->
                             run {
                                 dialog.dismiss()
                                 viewModel.startGame()
+                            }
+                        }.setNeutralButton("Share") {
+                            dialog, _ ->
+                            run {
+                                dialog.dismiss()
+                                //todo: check permissions
+                                shareScreenShot(takeScreenShot(game_frame))
                             }
                         }.setCancelable(
                             false
@@ -101,5 +115,46 @@ class SnakeGameActivity : AppCompatActivity() {
         }
 
         viewModel.startGame()
+    }
+
+    private fun takeScreenShot(view: View): Bitmap {
+        val returnBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnBitmap)
+        view.draw(canvas)
+        return returnBitmap
+    }
+
+    private fun shareScreenShot(resource: Bitmap) {
+        //todo: do export in coroutine
+        var path: String
+        try {
+            val bytes = ByteArrayOutputStream()
+            resource.compress(Bitmap.CompressFormat.PNG, 100, bytes)
+            val file = File(
+                "/storage/emulated/0/DCIM/Camera"
+                        + File.separator + "Snake_"
+                        + System.currentTimeMillis() / 1000 + ".png"
+            )
+
+            val fo = FileOutputStream(file)
+            fo.write(bytes.toByteArray())
+            fo.close()
+            path = file.absolutePath
+        } catch (e: IOException) {
+            path = ""
+            e.printStackTrace()
+        }
+
+
+        MediaScannerConnection.scanFile(this, arrayOf(path), null) { _, uri ->
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+            shareIntent.type = "image/png"
+
+            startActivity(
+                Intent.createChooser(shareIntent, "Share")
+            )
+        }
     }
 }
