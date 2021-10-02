0d6a549b4bc0de14c17e66624ff1b8191aeb74b5
rikucherry1993 <rikucherry0522@gmail.com>
2021-10-02 12:23:45 +0900

    [WIP] Create espresso test class

---
+++ app/src/androidTest/java/com/rikucherry/simplesnake/SnakeGameActivityTest.kt
@@ -0,0 +1,18 @@
+package com.rikucherry.simplesnake
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
+import org.junit.Before
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SnakeGameActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
